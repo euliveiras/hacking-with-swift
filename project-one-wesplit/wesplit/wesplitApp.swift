9fb56257746c573a3409db7af46de3b8471ca0b7
Matheus Oliveira <matheus.skm@hotmail.com>
2023-02-09 11:33:31 -0300

    project one

---
+++ project-one-wesplit/wesplit/wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  wesplitApp.swift
+//  wesplit
+//
+//  Created by Matheus Oliveira  on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
